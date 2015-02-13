2cd5d52e1f5eeb80c7162bd3f4581a817cbfab36
Charles Hache <charles.hache@gmail.com>
2015-02-13 03:02:42 -0800

    Fixing line endings, adding copyright

--- src/ca/brood/projector/SubclassType.java
+++ src/ca/brood/projector/SubclassType.java
@@ -1,3 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Charles Hache <chalz@member.fsf.org>. All rights reserved. 
+ * 
+ * This file is part of the projector project.
+ * projector is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * projector is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with projector.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Charles Hache <chalz@member.fsf.org> - initial API and implementation
+ ******************************************************************************/
 package ca.brood.projector;
 
 import ca.brood.projector.base.BaseSubclassType;
